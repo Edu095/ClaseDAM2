7bfe30cfd51c589a9d2a9eb7153ae3096bc961b4
Edu <Edu@MacEdu.local>
2016-09-27 17:57:29 +0200

    Exercici4 de VB

---
+++ VisualBasic/Bloc_Exercici4/Bloc_Exercici4/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num As Double = CDbl(TextBox1.Text)
+        Dim xiv As Boolean = False
+
+        If num < 50 Then
+            MsgBox(num ^ 2)
+            xiv = True
+        End If
+        If num = 25 Or num = 30 Or num = 75 Then
+            MsgBox(num ^ (1 / 2))
+            xiv = True
+        End If
+        If num = 10 Or num > 100 Or (num > 3.8 And num < 77.9) Then
+            MsgBox(num / 10)
+            xiv = True
+        End If
+        If xiv=False Then
+            MsgBox(num)
+        End If
+
+    End Sub
+End Class
