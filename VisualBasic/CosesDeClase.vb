0e4b50202c5b0cfae965d95ac737c5b171634ffd
Edu <eduojerb@gmail.com>
2016-10-13 16:54:34 +0200

    Coses practiques que esplique a clase

---
+++ VisualBasic/CosesDeClase.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim aleatori As New Random
+        Static a As Integer
+        Dim b As Integer
+        TextBox1.Text = aleatori.Next(10, 21)
+        b = 5
+        MsgBox(Str(Module1.n) + ", " + Str(a) + ", " + Str(b))
+        a += 1
+        b += 1
+        Module1.n += 1
+
+        Form2.Show()
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
