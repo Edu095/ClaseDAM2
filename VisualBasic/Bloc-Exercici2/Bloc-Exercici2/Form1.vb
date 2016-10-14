27ebb93d68f5cb6dfd128dd6489f22db374fd043
Edu <eduojerb@gmail.com>
2016-10-14 18:20:38 +0200

    Modificacio Bloc2, resolució arrel cuadrada

--- VisualBasic/Bloc-Exercici2/Bloc-Exercici2/Form1.vb
+++ VisualBasic/Bloc-Exercici2/Bloc-Exercici2/Form1.vb
@@ -1,13 +1,28 @@
-﻿Public Class Form1
+﻿Imports System.Math
+Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim vA As Double = CDbl(VarA.Text)
-        Dim vB As Double = CDbl(VarB.Text)
-        Dim vC As Double = CDbl(VarC.Text)
+        Dim vA As Double
+        Dim vB As Double
+        Dim vC As Double
+        Dim a As Double
 
-        Poli.Text = VarA.Text + "x^2 + " + VarB.Text + "x + " + VarC.Text
+        If VarA.Text = "" Or VarB.Text = "" Or VarC.Text = "" Then
+            MsgBox("Has d'introduir tots els parametres!!")
+        Else
+            vA = CDbl(VarA.Text)
+            vB = CDbl(VarB.Text)
+            vC = CDbl(VarC.Text)
+            a = vB ^ 2 - 4 * vA * vC
 
-        ResulPos.Text = (-vB + (vB ^ 2 - 4 * vA * vC) ^ (1 / 2)) / 2 * vA
-        ResulNeg.Text = (-vB - (vB ^ 2 - 4 * vA * vC) ^ (1 / 2)) / 2 * vA
+            Poli.Text = VarA.Text + "x^2 + " + VarB.Text + "x + " + VarC.Text
+
+            If a <= 0 Then
+                MsgBox("No es pot resoldre")
+            Else
+                ResulNeg.Text = (-vB - Sqrt(a)) / (2 * vA)
+                ResulPos.Text = (-vB + Sqrt(a)) / (2 * vA)
+            End If
+        End If
 
     End Sub
 End Class
