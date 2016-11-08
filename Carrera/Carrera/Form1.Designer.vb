4a3c1c654b966e399ae27d36b0f7de9d3db0cd98
Edu <eduojerb@gmail.com>
2016-11-08 16:15:14 +0100

    Carrera de imatges

---
+++ Carrera/Carrera/Form1.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "A corre!!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "grass-bg.jpg")
+        Me.ImageList1.Images.SetKeyName(1, "award_star_gold_3.ico")
+        Me.ImageList1.Images.SetKeyName(2, "award_star_silver_3.ico")
+        Me.ImageList1.Images.SetKeyName(3, "award_star_bronze_3.ico")
+        Me.ImageList1.Images.SetKeyName(4, "bug.ico")
+        Me.ImageList1.Images.SetKeyName(5, "cake.ico")
+        Me.ImageList1.Images.SetKeyName(6, "car.ico")
+        Me.ImageList1.Images.SetKeyName(7, "emoticon_waii.ico")
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Carrera.My.Resources.Resources.grass_bg
+        Me.ClientSize = New System.Drawing.Size(974, 378)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ImageList1 As ImageList
+End Class
---
+++ Carrera/Carrera/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Dim MyGraphics As Graphics = Me.CreateGraphics
+        Dim rotulador As Pen
+        Dim i As Integer
+        Dim top As Integer = 65
+        Dim bot As Integer = 78
+        rotulador = New Pen(Brushes.LightBlue, 4)
+        MyGraphics.DrawLine(rotulador, 20, 65, 900, 65)
+        MyGraphics.DrawLine(rotulador, 20, 335, 900, 335)
+        rotulador = New Pen(Brushes.Black, 15)
+        MyGraphics.DrawLine(rotulador, 900, 52, 900, 348)
+        rotulador = New Pen(Brushes.White, 15)
+        For i = 1 To 11
+            MyGraphics.DrawLine(rotulador, 900, top, 900, bot)
+            top += 26
+            bot += 26
+            Application.DoEvents()
+        Next
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
